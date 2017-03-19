2af4d20c32f19d57a9a4c024617b67befff6fdfd
lklabunde <lucas.klabunde@gmx.de>
2017-03-19 16:47:50 +0100

    GUI

---
+++ Fahrscheinautomat/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Fahrscheinautomat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbx_Fahrscheine = new System.Windows.Forms.GroupBox();
+            this.btn_Fahrkarte4 = new System.Windows.Forms.Button();
+            this.btn_Fahrkarte3 = new System.Windows.Forms.Button();
+            this.btn_Fahrkarte2 = new System.Windows.Forms.Button();
+            this.btn_Fahrkarte1 = new System.Windows.Forms.Button();
+            this.gbx_Wechselgeld = new System.Windows.Forms.GroupBox();
+            this.btn_2euro = new System.Windows.Forms.Button();
+            this.btn_1euro = new System.Windows.Forms.Button();
+            this.btn_50ct = new System.Windows.Forms.Button();
+            this.btn_20ct = new System.Windows.Forms.Button();
+            this.btn_10ct = new System.Windows.Forms.Button();
+            this.lbl_Status = new System.Windows.Forms.Label();
+            this.gbx_Status = new System.Windows.Forms.GroupBox();
+            this.btn_Technikerinterface = new System.Windows.Forms.Button();
+            this.btn_Ende = new System.Windows.Forms.Button();
+            this.gbx_Fahrscheine.SuspendLayout();
+            this.gbx_Wechselgeld.SuspendLayout();
+            this.gbx_Status.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbx_Fahrscheine
+            // 
+            this.gbx_Fahrscheine.Controls.Add(this.btn_Fahrkarte4);
+            this.gbx_Fahrscheine.Controls.Add(this.btn_Fahrkarte3);
+            this.gbx_Fahrscheine.Controls.Add(this.btn_Fahrkarte2);
+            this.gbx_Fahrscheine.Controls.Add(this.btn_Fahrkarte1);
+            this.gbx_Fahrscheine.Location = new System.Drawing.Point(12, 12);
+            this.gbx_Fahrscheine.Name = "gbx_Fahrscheine";
+            this.gbx_Fahrscheine.Size = new System.Drawing.Size(619, 632);
+            this.gbx_Fahrscheine.TabIndex = 0;
+            this.gbx_Fahrscheine.TabStop = false;
+            this.gbx_Fahrscheine.Text = "Fahrscheine";
+            // 
+            // btn_Fahrkarte4
+            // 
+            this.btn_Fahrkarte4.Location = new System.Drawing.Point(312, 325);
+            this.btn_Fahrkarte4.Name = "btn_Fahrkarte4";
+            this.btn_Fahrkarte4.Size = new System.Drawing.Size(300, 300);
+            this.btn_Fahrkarte4.TabIndex = 3;
+            this.btn_Fahrkarte4.Text = "Fahrkarte4";
+            this.btn_Fahrkarte4.UseVisualStyleBackColor = true;
+            // 
+            // btn_Fahrkarte3
+            // 
+            this.btn_Fahrkarte3.Location = new System.Drawing.Point(6, 325);
+            this.btn_Fahrkarte3.Name = "btn_Fahrkarte3";
+            this.btn_Fahrkarte3.Size = new System.Drawing.Size(300, 300);
+            this.btn_Fahrkarte3.TabIndex = 2;
+            this.btn_Fahrkarte3.Text = "Fahrkarte3";
+            this.btn_Fahrkarte3.UseVisualStyleBackColor = true;
+            // 
+            // btn_Fahrkarte2
+            // 
+            this.btn_Fahrkarte2.Location = new System.Drawing.Point(312, 19);
+            this.btn_Fahrkarte2.Name = "btn_Fahrkarte2";
+            this.btn_Fahrkarte2.Size = new System.Drawing.Size(300, 300);
+            this.btn_Fahrkarte2.TabIndex = 1;
+            this.btn_Fahrkarte2.Text = "Fahrkarte2";
+            this.btn_Fahrkarte2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Fahrkarte1
+            // 
+            this.btn_Fahrkarte1.Location = new System.Drawing.Point(6, 19);
+            this.btn_Fahrkarte1.Name = "btn_Fahrkarte1";
+            this.btn_Fahrkarte1.Size = new System.Drawing.Size(300, 300);
+            this.btn_Fahrkarte1.TabIndex = 0;
+            this.btn_Fahrkarte1.Text = "Fahrkarte1";
+            this.btn_Fahrkarte1.UseVisualStyleBackColor = true;
+            // 
+            // gbx_Wechselgeld
+            // 
+            this.gbx_Wechselgeld.Controls.Add(this.btn_10ct);
+            this.gbx_Wechselgeld.Controls.Add(this.btn_20ct);
+            this.gbx_Wechselgeld.Controls.Add(this.btn_50ct);
+            this.gbx_Wechselgeld.Controls.Add(this.btn_1euro);
+            this.gbx_Wechselgeld.Controls.Add(this.btn_2euro);
+            this.gbx_Wechselgeld.Location = new System.Drawing.Point(638, 12);
+            this.gbx_Wechselgeld.Name = "gbx_Wechselgeld";
+            this.gbx_Wechselgeld.Size = new System.Drawing.Size(537, 125);
+            this.gbx_Wechselgeld.TabIndex = 1;
+            this.gbx_Wechselgeld.TabStop = false;
+            this.gbx_Wechselgeld.Text = "Wechselgeld";
+            // 
+            // btn_2euro
+            // 
+            this.btn_2euro.Location = new System.Drawing.Point(6, 19);
+            this.btn_2euro.Name = "btn_2euro";
+            this.btn_2euro.Size = new System.Drawing.Size(100, 100);
+            this.btn_2euro.TabIndex = 0;
+            this.btn_2euro.Text = "2Euro";
+            this.btn_2euro.UseVisualStyleBackColor = true;
+            // 
+            // btn_1euro
+            // 
+            this.btn_1euro.Location = new System.Drawing.Point(112, 19);
+            this.btn_1euro.Name = "btn_1euro";
+            this.btn_1euro.Size = new System.Drawing.Size(100, 100);
+            this.btn_1euro.TabIndex = 1;
+            this.btn_1euro.Text = "1Euro";
+            this.btn_1euro.UseVisualStyleBackColor = true;
+            // 
+            // btn_50ct
+            // 
+            this.btn_50ct.Location = new System.Drawing.Point(218, 19);
+            this.btn_50ct.Name = "btn_50ct";
+            this.btn_50ct.Size = new System.Drawing.Size(100, 100);
+            this.btn_50ct.TabIndex = 2;
+            this.btn_50ct.Text = "50ct";
+            this.btn_50ct.UseVisualStyleBackColor = true;
+            // 
+            // btn_20ct
+            // 
+            this.btn_20ct.Location = new System.Drawing.Point(324, 19);
+            this.btn_20ct.Name = "btn_20ct";
+            this.btn_20ct.Size = new System.Drawing.Size(100, 100);
+            this.btn_20ct.TabIndex = 3;
+            this.btn_20ct.Text = "20ct";
+            this.btn_20ct.UseVisualStyleBackColor = true;
+            // 
+            // btn_10ct
+            // 
+            this.btn_10ct.Location = new System.Drawing.Point(430, 19);
+            this.btn_10ct.Name = "btn_10ct";
+            this.btn_10ct.Size = new System.Drawing.Size(100, 100);
+            this.btn_10ct.TabIndex = 4;
+            this.btn_10ct.Text = "10ct";
+            this.btn_10ct.UseVisualStyleBackColor = true;
+            // 
+            // lbl_Status
+            // 
+            this.lbl_Status.AutoSize = true;
+            this.lbl_Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Status.Location = new System.Drawing.Point(212, 81);
+            this.lbl_Status.Name = "lbl_Status";
+            this.lbl_Status.Size = new System.Drawing.Size(92, 31);
+            this.lbl_Status.TabIndex = 2;
+            this.lbl_Status.Text = "Status";
+            // 
+            // gbx_Status
+            // 
+            this.gbx_Status.Controls.Add(this.lbl_Status);
+            this.gbx_Status.Location = new System.Drawing.Point(638, 144);
+            this.gbx_Status.Name = "gbx_Status";
+            this.gbx_Status.Size = new System.Drawing.Size(539, 187);
+            this.gbx_Status.TabIndex = 3;
+            this.gbx_Status.TabStop = false;
+            this.gbx_Status.Text = "Status";
+            // 
+            // btn_Technikerinterface
+            // 
+            this.btn_Technikerinterface.Location = new System.Drawing.Point(637, 337);
+            this.btn_Technikerinterface.Name = "btn_Technikerinterface";
+            this.btn_Technikerinterface.Size = new System.Drawing.Size(540, 148);
+            this.btn_Technikerinterface.TabIndex = 4;
+            this.btn_Technikerinterface.Text = "Technikerinterface";
+            this.btn_Technikerinterface.UseVisualStyleBackColor = true;
+            // 
+            // btn_Ende
+            // 
+            this.btn_Ende.Location = new System.Drawing.Point(638, 491);
+            this.btn_Ende.Name = "btn_Ende";
+            this.btn_Ende.Size = new System.Drawing.Size(540, 146);
+            this.btn_Ende.TabIndex = 5;
+            this.btn_Ende.Text = "Ende";
+            this.btn_Ende.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1189, 649);
+            this.Controls.Add(this.btn_Ende);
+            this.Controls.Add(this.btn_Technikerinterface);
+            this.Controls.Add(this.gbx_Status);
+            this.Controls.Add(this.gbx_Wechselgeld);
+            this.Controls.Add(this.gbx_Fahrscheine);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbx_Fahrscheine.ResumeLayout(false);
+            this.gbx_Wechselgeld.ResumeLayout(false);
+            this.gbx_Status.ResumeLayout(false);
+            this.gbx_Status.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbx_Fahrscheine;
+        private System.Windows.Forms.Button btn_Fahrkarte4;
+        private System.Windows.Forms.Button btn_Fahrkarte3;
+        private System.Windows.Forms.Button btn_Fahrkarte2;
+        private System.Windows.Forms.Button btn_Fahrkarte1;
+        private System.Windows.Forms.GroupBox gbx_Wechselgeld;
+        private System.Windows.Forms.Button btn_10ct;
+        private System.Windows.Forms.Button btn_20ct;
+        private System.Windows.Forms.Button btn_50ct;
+        private System.Windows.Forms.Button btn_1euro;
+        private System.Windows.Forms.Button btn_2euro;
+        private System.Windows.Forms.Label lbl_Status;
+        private System.Windows.Forms.GroupBox gbx_Status;
+        private System.Windows.Forms.Button btn_Technikerinterface;
+        private System.Windows.Forms.Button btn_Ende;
+    }
+}
+
